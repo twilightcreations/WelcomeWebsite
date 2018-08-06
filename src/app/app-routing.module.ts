cebc54a205196130198fb8afd733c6c74895314f
Bhatti <Parminder.Bhatti@linkgroup.co.uk>
2018-08-06 12:10:27 +0100

    added links to the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FeesComponent } from './pages/fees/fees.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SubjectsOfferedComponent } from './pages/subjects-offered/subjects-offered.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: '', component: HomeComponent },
+    { path: 'homepage', component: HomeComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'faqs', component: FaqsComponent },
+    { path: 'fees', component: FeesComponent },
+    { path: 'subjects-offered', component: SubjectsOfferedComponent },
+    { path: 'testimonials', component: TestimonialsComponent },
+    { path: 'contact-us', component: ContactUsComponent },
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
